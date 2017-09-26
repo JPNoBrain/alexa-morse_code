b774b83a30a67d1f92a074c14164c3c95ffa1164
Jascha Dietz <jaschadietz@live.de>
2017-09-26 08:42:27 +0200

    Added string splitting snytax

--- Alexa-morse_code/Alexa-morse_code/Function.cs
+++ Alexa-morse_code/Alexa-morse_code/Function.cs
@@ -5,12 +5,30 @@ using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 using Alexa.NET.Response;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Request;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace Alexa_morse_code
 {
+    public class Messages
+    {
+        public Messages(string language)
+        {
+            this.Language = language;
+        }
+
+        public string Language { get; set; }
+        public string SkillName { get; set; }
+        public string HelpMessage { get; set; }
+        public string HelpReprompt { get; set; }
+        public string StopMessage { get; set; }
+        public string GetMorseCode { get; set; }
+        public string LaunchMessage { get; set; }
+    }
+
     public class Function
     {
 
@@ -20,25 +38,116 @@ namespace Alexa_morse_code
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public /*string*/ SkillResponse FunctionHandler(string input, ILambdaContext context)
+        public List<Messages> GetResources()
+        {
+            List<Messages> resources = new List<Messages>();
+            Messages enUSResource = new Messages("en-US");
+            enUSResource.SkillName = "Morse Code Translator";
+            enUSResource.GetMorseCode = "Here's your morse code: ";
+            enUSResource.HelpMessage = "You can say Alexa, morse hello, or, you can say exit... What can I help you with?";
+            enUSResource.HelpReprompt = "What can I help you with?";
+            enUSResource.StopMessage = "Goodbye!";
+            enUSResource.LaunchMessage = "Welcome to Morse Translator. Please provide the desired text to be morsed.";
+            resources.Add(enUSResource);
+            return resources;
+        }
+
+        public /*string*/ SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
             SkillResponse response = new SkillResponse();
             response.Response.ShouldEndSession = false;
             IOutputSpeech innerResponse = null;
             var log = context.Logger;
 
-            var allResources = GetResources();
-            var resource = allResources.FirstOrDefault();
-            return input?.ToUpper();
+            if (input.GetRequestType() == typeof(LaunchRequest))
+            {
+                log.LogLine($"Default LaunchRequest made: 'Alexa, open Science Facts");
+                innerResponse = new PlainTextOutputSpeech();
+                (innerResponse as PlainTextOutputSpeech).Text = GetResources()[0].LaunchMessage;
+
+            }
+            else if (input.GetRequestType() == typeof(IntentRequest))
+            {
+                var intentRequest = (IntentRequest)input.Request;
+                switch (intentRequest.Intent.Name)
+                {
+                    case "AMAZON.CancelIntent":
+                        log.LogLine($"AMAZON.CancelIntent: send StopMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = GetResources()[0].StopMessage;
+                        response.Response.ShouldEndSession = true;
+                        break;
+                    case "AMAZON.StopIntent":
+                        log.LogLine($"AMAZON.StopIntent: send StopMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = GetResources()[0].StopMessage;
+                        response.Response.ShouldEndSession = true;
+                        break;
+                    case "AMAZON.HelpIntent":
+                        log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = GetResources()[0].HelpMessage;
+                        break;
+                    case "GetFactIntent":
+                        log.LogLine($"GetFactIntent sent: send new fact");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = ""; //TODO
+                        break;
+                    case "GetNewFactIntent":
+                        log.LogLine($"GetFactIntent sent: send new fact");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = ""; //TODO
+                        break;
+                    default:
+                        log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = GetResources()[0].HelpReprompt;
+                        break;
+                }
+            }
+            response.Response.OutputSpeech = innerResponse;
+            response.Version = "1.0";
+            return response;
         }
 
-        public char[] toArray(string input)
+        public char[] ToArray(string input)
         {
             char[] characters = input.ToCharArray();
             return characters;
         }
 
-        public void output(char[] input)
+        public List<char> Translate(char[] letters)
+        {
+            List<char> morse = new List<char>();
+            for(int i = 0;i < letters.Length;i++)
+            {
+                List<char> temp = new List<char>();
+                //Get translation from letters[i]
+                //DB tutorial: http://matthiasshapiro.com/2017/03/21/tutorial-dynamodb-in-net/
+                for (int j = 0;j < temp.Capacity; j++)
+                {
+                    morse.Add(temp.ElementAt(0));
+                    if(j != temp.Capacity - 1)
+                    {
+                        morse.Add(',');
+                    }
+                }
+                if (i != letters.Length - 1)
+                {
+                    if (letters[i + 1] == ' ')
+                    {
+                        morse.Add('|');
+                    }
+                    else
+                    {
+                        morse.Add('+');
+                    }
+                }
+            }
+            return morse;
+        }
+
+        public void Output(char[] input)
         {
             //TODO
         }
