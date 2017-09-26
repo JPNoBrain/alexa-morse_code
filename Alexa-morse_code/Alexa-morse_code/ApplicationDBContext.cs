727c54772a593f03d793aa0dfc08013355185cbc
Pascal Kröger <pascal.kroeger@obungi.com>
2017-09-26 10:05:07 +0200

    Added Nuget Package AWS DynamoDB

---
+++ Alexa-morse_code/Alexa-morse_code/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
+
+namespace Alexamorse_code
+{
+    public class ApplicationDBContext
+    {
+        var credentials = new BasicAWSCredentials(accessKey, secretKey);
+        var client = new AmazonDynamoDBClient(credentials, RegionEndpoint.EUCentral1);
+    }
+}
